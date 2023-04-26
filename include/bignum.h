30b2ea7f082ef7a896d3ece8a1681d5b9e311082
Alphaly <1990369867@qq.com>
2023-04-26 12:15:23 +0800

    Update bignum.h

--- include/bignum.h
+++ include/bignum.h
@@ -1,9 +1,12 @@
 #pragma once
 namespace alp{
-        class BigNumber{
-        public:
+class BigNumber{
+        private:
         char* number;
         int adv;
+        public:
+        static char* GetTotal(char*,char*,int);
+        static int GetDec(char);
         BigNumber();
         ~BigNumber();
         BigNumber(char*,int);
@@ -11,8 +14,18 @@ namespace alp{
         BigNumber Add(char*,int);
         BigNumber Add(int);
         BigNumber operator+(const BigNumber);
-        public:
-        static char* GetTotal(char*,char*,int);
-        static int GetDec(char);
 }; 
+class RadixNotEqualException{
+        public:
+        RadixNotEqualException(int adva,int advb):m_adva(adva),m_advb(advb) {
+                if(m_adva==m_advb) return;
+                if(m_adva>m_advb) m_flag=1;
+                else m_flag=2;
+        };
+        void Print() const;
+        private:
+        int m_flag;
+        int m_adva;
+        int m_advb;
+};
 }
