472cc3ba30c76f5f44665b0121d8f817cfba6cd3
Alphaly <1990369867@qq.com>
2023-04-28 09:22:09 +0000

    Big Decimical class

--- include/bignum.h
+++ include/bignum.h
@@ -1,31 +1,49 @@
 #pragma once
-namespace alp{
-class BigNumber{
+namespace alp
+{
+        class BigNumber
+        {
         private:
-        char* number;
-        int adv;
+                char *number;
+                int radi;
+                static char *GetTotal(char *, char *, int);
+                static int GetDec(char);
+                BigNumber Add(char *, int);
+
         public:
-        static char* GetTotal(char*,char*,int);
-        static int GetDec(char);
-        BigNumber();
-        ~BigNumber();
-        BigNumber(char*,int);
-        BigNumber Add(BigNumber);
-        BigNumber Add(char*,int);
-        BigNumber Add(int);
-        BigNumber operator+(const BigNumber);
-}; 
-class RadixNotEqualException{
-        public:
-        RadixNotEqualException(int adva,int advb):m_adva(adva),m_advb(advb) {
-                if(m_adva==m_advb) return;
-                if(m_adva>m_advb) m_flag=1;
-                else m_flag=2;
+                BigNumber();
+                BigNumber(char *, int);
+                BigNumber Add(BigNumber);
+                BigNumber Add(int);
+                BigNumber operator+(const BigNumber);
         };
-        void Print() const;
+        class BigDecimical : BigNumber
+        {
         private:
-        int m_flag;
-        int m_adva;
-        int m_advb;
-};
+                char *number;
+                int radi = 10;
+
+        public:
+                BigDecimical();
+                BigDecimical(char *);
+        };
+        class RadixNotEqualException
+        {
+        public:
+                RadixNotEqualException(int adva, int advb) : m_adva(adva), m_advb(advb)
+                {
+                        if (m_adva == m_advb)
+                                return;
+                        if (m_adva > m_advb)
+                                m_flag = 1;
+                        else
+                                m_flag = 2;
+                };
+                void Print() const;
+
+        private:
+                int m_flag;
+                int m_adva;
+                int m_advb;
+        };
 }
--- src/bignum.cpp
+++ src/bignum.cpp
@@ -1,92 +1,196 @@
 #include "bignum.h"
-#include<iostream>
-#include<cstring>
+#include <iostream>
+#include <cstring>
 
-using std::cin;
-using std::cout;
-using std::isalpha;
-using std::strlen;
-using std::endl;
+using alp::BigDecimical;
 using alp::BigNumber;
 using alp::RadixNotEqualException;
-int BigNumber::GetDec(char ch){
-	if(isalpha(ch)) return ch-'A'+10;
-	else return ch-'0';
+using std::cin;
+using std::cout;
+using std::endl;
+using std::isalpha;
+using std::strlen;
+#pragma region BigNumber class
+/// @brief Decimical
+/// @param ch
+/// @return
+int BigNumber::GetDec(char ch)
+{
+	if (isalpha(ch))
+		return ch - 'A' + 10;
+	else
+		return ch - '0';
 }
-
-char* BigNumber::GetTotal(char* a,char* b,int adv){
-	const char* output="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	const int lena=strlen(a);
-	const int lenb=strlen(b);
-    int m[lena+1],n[lenb+1],tmpAns[lenb+1];
-	int maxLen=(lena<lenb)?lenb:lena;
-	for(int i=0;i<lena;i++) m[i]=GetDec(a[lena-i-1]);
-	for(int i=0;i<lenb;i++) n[i]=GetDec(b[lenb-i-1]);
-	for(int i=0;i<maxLen;i++){
-		tmpAns[i]+=m[i]+n[i]; 
-		tmpAns[i+1]=tmpAns[i]/adv;
-		tmpAns[i]%=adv;
+/// @brief Itoa int to array
+/// @param num
+/// @param str
+/// @param radix
+/// @return The pointer of the result
+char *itoa(int num, char *str, int radix)
+{
+	char index[] = "0123456789ABCDEF";
+	unsigned unum;
+	int i = 0, j, k;
+	if (radix == 10 && num < 0)
+	{
+		unum = (unsigned)-num;
+		str[i++] = '-';
 	}
-	if(tmpAns[maxLen]) maxLen++;
-    char* Ans =new char[maxLen];
-	for(int i=maxLen-1;i>=0;i--){
-		Ans[maxLen-i-1]=char(output[tmpAns[i]]);
-	} 
-    return Ans;
-}
+	else
+		unum = (unsigned)num;
+	do
+	{
+		str[i++] = index[unum % (unsigned)radix];
+		unum /= radix;
+	} while (unum);
+	str[i] = '\0';
+	if (str[0] == '-')
+		k = 1;
+	else
+		k = 0;
 
-BigNumber::BigNumber(){
-	number=new char[1];
-	number[0]='0';
-	adv=10;
+	for (j = k; j <= (i - 1) / 2; j++)
+	{
+		char temp;
+		temp = str[j];
+		str[j] = str[i - 1 + k - j];
+		str[i - 1 + k - j] = temp;
+	}
+	return str;
 }
-BigNumber::BigNumber(char* num,int nadv=10){
-	int numlen=strlen(num);
-	number=new char[numlen];
-	adv=nadv;
+/// @brief Get the result of a and b(radix=radi)
+/// @param a
+/// @param b
+/// @param radi
+/// @return char*
+char *BigNumber::GetTotal(char *a, char *b, int radi)
+{
+	const char *output = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	const int lena = strlen(a);
+	const int lenb = strlen(b);
+	int m[lena + 1], n[lenb + 1], tmpAns[lenb + 1];
+	int maxLen = (lena < lenb) ? lenb : lena;
+	for (int i = 0; i < lena; i++)
+		m[i] = GetDec(a[lena - i - 1]);
+	for (int i = 0; i < lenb; i++)
+		n[i] = GetDec(b[lenb - i - 1]);
+	for (int i = 0; i < maxLen; i++)
+	{
+		tmpAns[i] += m[i] + n[i];
+		tmpAns[i + 1] = tmpAns[i] / radi;
+		tmpAns[i] %= radi;
+	}
+	if (tmpAns[maxLen])
+		maxLen++;
+	char *Ans = new char[maxLen];
+	for (int i = maxLen - 1; i >= 0; i--)
+	{
+		Ans[maxLen - i - 1] = char(output[tmpAns[i]]);
+	}
+	return Ans;
 }
-
-BigNumber BigNumber::Add(BigNumber num){
-	try{
-		return *this+num;
-	}catch(RadixNotEqualException& e){
+/// @brief Default construction function
+BigNumber::BigNumber()
+{
+	number = new char[1];
+	number[0] = '0';
+	radi = 10;
+}
+/// @brief BigNumber construction function
+/// @param num
+/// @param nadv
+BigNumber::BigNumber(char *num, int nradi = 10)
+{
+	int numlen = strlen(num);
+	number = new char[numlen];
+	radi = nradi;
+}
+/// @brief BigNumber add with BigNumber
+/// @param num
+/// @return BigNumber class
+BigNumber BigNumber::Add(BigNumber num)
+{
+	try
+	{
+		return *this + num;
+	}
+	catch (RadixNotEqualException &e)
+	{
 		e.Print();
-		return BigNumber(0,10);
+		return BigNumber(0, 10);
 	}
-
 }
-BigNumber BigNumber::Add(char* num,int nadv){
-	BigNumber newBN(num,nadv);
-	try{
-		return *this+newBN;
-	}catch(RadixNotEqualException& e){
+/// @brief BigNumber add with char*
+/// @param num
+/// @param nradi
+/// @return BigNumber class
+BigNumber BigNumber::Add(char *num, int nradi)
+{
+	BigNumber newBN(num, nradi);
+	try
+	{
+		return *this + newBN;
+	}
+	catch (RadixNotEqualException &e)
+	{
 		e.Print();
-		return BigNumber(0,10);
+		return BigNumber(0, 10);
 	}
 }
-BigNumber BigNumber::Add(int num){
-	//To-do
-	return 0;
+/// @brief BigNumber add with integer
+/// @param num
+/// @return BigNumber class
+BigNumber BigNumber::Add(int num)
+{
+	char snum[32];
+	itoa(num, snum, this->radi);
+	BigNumber newBN(snum, this->radi);
+	return *this + newBN;
 }
-BigNumber BigNumber::operator+(const BigNumber num){
-	BigNumber newBN(num.number,num.adv);
-	if(this->adv==num.adv)
-		newBN.number=BigNumber::GetTotal(this->number,newBN.number,num.adv);
-	else{
-		throw RadixNotEqualException(this->adv,num.adv);	//Use try-catch to detect RadixNotEqualException.
+/// @brief Operator+ override
+/// @param num
+/// @return BigNumber class
+BigNumber BigNumber::operator+(const BigNumber num)
+{
+	BigNumber newBN(num.number, num.radi);
+	if (this->radi == num.radi)
+		newBN.number = BigNumber::GetTotal(this->number, newBN.number, num.radi);
+	else
+	{
+		throw RadixNotEqualException(this->radi, num.radi); // Use try-catch to detect RadixNotEqualException.
 	}
 	return newBN;
 }
-void RadixNotEqualException::Print() const{
+#pragma endregion
+#pragma region BigDecimical class
+/// @brief Default construction function
+BigDecimical::BigDecimical()
+{
+	number = new char[1];
+	number[0] = '0';
+}
+/// @brief BigDecimical construction function
+/// @param num
+BigDecimical::BigDecimical(char *num)
+{
+	int numlen = strlen(num);
+	number = new char[numlen];
+}
+#pragma endregion
+#pragma region RadixNotEqualExeption class
+/// Print the exception
+void RadixNotEqualException::Print() const
+{
 	switch (m_flag)
 	{
 	case 1:
-		cout<<"The radix of the value left is bigger than that of the right one."<<endl;
+		cout << "The radix of the value left is bigger than that of the right one." << endl;
 		break;
 	case 2:
-		cout<<"The radix of the value right is bigger than that of the left one."<<endl;
+		cout << "The radix of the value right is bigger than that of the left one." << endl;
 	default:
-		cout<<"Unknown exception detected."<<endl;
+		cout << "Unknown exception detected." << endl;
 		break;
 	}
 }
+#pragma endregion
